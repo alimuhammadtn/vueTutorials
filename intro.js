0b9b346c9c086a012c5e4a426043d6413629e503
alimuhammadtn <alimuhammadtn@gmail.com>
2018-01-03 13:58:41 +0400

    js file upload

---
+++ intro.js
@@ -0,0 +1,37 @@
+var obj = {
+          foo: 'bar'
+          }
+
+          //Object.freeze(obj)
+
+          new Vue({
+          el: '#app',
+          data () {
+          return {
+          obj
+          }
+          }
+          })
+
+Vue.component('todo-item', {
+  props: ['ali'],
+  template: '<li>{{ ali.text }}</li>'
+})
+}
+
+
+
+
+  
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+
